d5d0783092267a4fc6e632b3808d4f294282d3aa
king-dragon <utkarshchauhan17@outlook.com>
2021-06-28 10:26:07 +0530

    add: function for update and delete task

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 @Injectable()
 export class TaskService {
   private tasks: Task[] = [];
@@ -9,7 +10,13 @@ export class TaskService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
@@ -19,5 +26,25 @@ export class TaskService {
 
     this.tasks.push(task);
     return task;
+  }
+
+  deleteTaskById(id: string): boolean {
+    const index = this.tasks.findIndex((task) => task.id === id);
+    if (index !== -1) {
+      this.tasks = this.tasks.slice(index, 1);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  updateTaskStatusById(id: string, status: TaskStatus): Task | string {
+    const index = this.tasks.findIndex((task) => task.id === id);
+
+    if (index !== -1) {
+      this.tasks[index].status = status;
+      return this.tasks[index];
+    }
+    return 'Task not found';
   }
 }
