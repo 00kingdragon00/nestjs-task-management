73a1396e45ea4a905cede980ead103b87f525c5f
king-dragon <utkarshchauhan17@outlook.com>
2021-07-01 16:24:21 +0530

    create: jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { PassportStrategy } from '@nestjs/passport';
+import { Model } from 'mongoose';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { JwtPayload } from './jwt-payload.interface';
+import { User, UserDocument } from './schema/user.schema';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'topSecret51',
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const { username } = payload;
+    const user = await this.userModel.findOne({ username });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
