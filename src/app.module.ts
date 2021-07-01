b33e0e287384ff3a9ddbf7f9e66e9aa1c6115d7d
king-dragon <utkarshchauhan17@outlook.com>
2021-07-01 10:42:13 +0530

    create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TaskModule,
     MongooseModule.forRoot('mongodb://localhost:27017/demo'),
+    AuthModule,
   ],
 })
 export class AppModule {}
