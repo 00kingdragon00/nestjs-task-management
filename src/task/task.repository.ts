da2f6cb026b0b80e0b695fed4a8fc1482e2d7a8e
king-dragon <utkarshchauhan17@outlook.com>
2021-07-01 10:44:54 +0530

    add: function for task create and get

--- src/task/task.repository.ts
+++ src/task/task.repository.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { FilterQuery, Model } from 'mongoose';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task, TaskDocument } from './schema/task.schema';
+import { TaskStatus } from './task-status.enum';
 
 @Injectable()
-export class UserRepository {
-  constructor(@InjectModel(Task.name) private userModel: Model<TaskDocument>) {}
+export class TaskRepository {
+  constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>) {}
+
+  async findOne(taskFilterQuery: FilterQuery<Task>): Promise<Task> {
+    return this.taskModel.findOne(taskFilterQuery);
+  }
+
+  async create(createTaskDto: CreateTaskDto): Promise<Task> {
+    const newTask = new this.taskModel();
+    newTask.title = createTaskDto.title;
+    newTask.description = createTaskDto.description;
+    newTask.status = TaskStatus.OPEN;
+    return newTask.save();
+  }
 }
