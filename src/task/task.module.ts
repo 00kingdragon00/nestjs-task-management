f6eea901267815a86fbdd4d84bff2d52249bc401
king-dragon <utkarshchauhan17@outlook.com>
2021-07-01 16:25:18 +0530

    import: authmodule

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
 import { Task, TaskSchema } from './schema/task.schema';
 import { TaskController } from './task.controller';
 import { TaskRepository } from './task.repository';
@@ -8,6 +9,7 @@ import { TaskService } from './task.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+    AuthModule,
   ],
   controllers: [TaskController],
   providers: [TaskService, TaskRepository],
