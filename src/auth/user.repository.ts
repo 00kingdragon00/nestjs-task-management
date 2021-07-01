f351847a35173f85ffee15c40ee744eeeedaf460
king-dragon <utkarshchauhan17@outlook.com>
2021-07-01 15:54:58 +0530

    add: user sign in and up function

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,9 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User, UserDocument } from './schema/user.schema';
-
+import { genSalt, hash } from 'bcrypt';
 @Injectable()
 export class UserRepository {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async signup(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = new this.userModel();
+
+    user.username = username;
+    user.salt = await genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+
+    try {
+      await user.save();
+    } catch (error) {
+      // duplicate user name
+      if (error.code === 11000) {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.userModel.findOne({ username });
+    const isValid = await user.validatePassword(password);
+    if (user && isValid) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return hash(password, salt);
+  }
 }
