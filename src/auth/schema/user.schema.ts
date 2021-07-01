80dda7a2c062130b8e48f460ce8cbbe65f837475
king-dragon <utkarshchauhan17@outlook.com>
2021-07-01 15:55:44 +0530

    add function for validatepassword

--- src/auth/schema/user.schema.ts
+++ src/auth/schema/user.schema.ts
@@ -1,15 +1,29 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { hash } from 'bcrypt';
 import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
-@Schema()
-export class User {
-  @Prop()
+@Schema({ timestamps: true })
+export class User extends Document {
+  @Prop({ unique: true })
   username: string;
 
   @Prop()
   password: string;
+
+  @Prop()
+  salt: string;
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  validatePassword: Function;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.methods.validatePassword = async function (
+  password: string,
+): Promise<boolean> {
+  const hashPassword = await hash(password, this.salt);
+  return hashPassword === this.password;
+};
