b53a3cb0ccece02ad50ec527508c87f14526ea4f
king-dragon <utkarshchauhan17@outlook.com>
2021-07-01 15:53:29 +0530

    import: jwt and passport module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User, UserSchema } from './schema/user.schema';
@@ -8,6 +10,13 @@ import { UserRepository } from './user.repository';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository],
